ccbbad7b993198201c76db7df9ff5038cd2ddd13
fmgambino <fernando.m.gambino@gmail.com>
2021-11-17 18:34:14 -0300

    Se cambio la url del WebSocket

--- application/views/scripts.php
+++ application/views/scripts.php
@@ -67,7 +67,7 @@ const options = {
 }
 
 // WebSocket connect url
-const WebSocket_URL = 'wss://ioticos.org:8094/mqtt';
+const WebSocket_URL = 'wss://webiot.com.ar:8094/mqtt';
 const client = mqtt.connect(WebSocket_URL, options)
 
 var device_topic = '<?php echo ROOT_TOPIC ."/". $_SESSION['selected_topic']."/" ?>';
