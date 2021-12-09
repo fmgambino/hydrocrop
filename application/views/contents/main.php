556ee595d7fbe3a7d4f43bdc2ec912bafe7da16f
fmgambino <fernando.m.gambino@gmail.com>
2021-12-09 20:10:31 -0300

    Se actualiza scripts.php

--- application/views/contents/main.php
+++ application/views/contents/main.php
@@ -247,7 +247,7 @@
                       <span class="slider round"></span>
                     </label>
                   </span>
-                  <h1><span class="red border br-5">2</span></h1>
+                  <h1><span class="red border hc-radio">2</span></h1>
                 </li>
 
                 <li class="mdl-list__item list__item--border-top">
