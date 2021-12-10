1face1d37446929cefeacb85f9cf686fcc762b3f
mauroz88 <mauro.zenteno150988@gmail.com>
2021-12-10 04:40:42 -0300

    se carga correcciones en main parametros

--- application/views/contents/main.php
+++ application/views/contents/main.php
@@ -494,7 +494,7 @@
     var datos = new FormData(formulario);
     if (datos.get('formtempmin') === "" || datos.get('formtempmax') === "" || datos.get('formhummin') === "" || datos.get('formhummax') === "" || datos.get('formco2min') === "" || datos.get('formco2max') === "" || datos.get('formtime') === "") {
       console.log('completar todos los campos');
-      alert('completar todos los campos');
+      Swal.fire('completar todos los campos')
     } 
     else {
       client.publish(device_topic + 'actions/ftempmin', datos.get('formtempmin'));
