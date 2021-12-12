3a28247746d12f1db3db311a7eb8139f8a512dab
fmgambino <fernando.m.gambino@gmail.com>
2021-12-12 01:07:49 -0300

    Se actualiza main.php

--- application/views/contents/main.php
+++ application/views/contents/main.php
@@ -68,44 +68,9 @@
               <i class="material-icons">local_fire_department</i>
               <div class="mdl-layout-spacer"></div>
             </div>
-            <div class="mdl-card__supporting-text mdl-card--expand">
-              <p id="display_co2" class="weather-temperature">--<sup>&deg;</sup></p>
-
-
-              <script type="text/javascript">
-                var css_file = document.createElement("link");
-                var widgetUrl = location.href;
-                css_file.setAttribute("rel", "stylesheet");
-                css_file.setAttribute("type", "text/css");
-                css_file.setAttribute("href", 'https://s.bookcdn.com/css/w/booked-wzs-widget-prime-days.css?v=0.0.1');
-                document.getElementsByTagName("head")[0].appendChild(css_file);
-
-                function setWidgetData_99676(data) {
-                  if (typeof(data) != 'undefined' && data.results.length > 0) {
-                    for (var i = 0; i < data.results.length; ++i) {
-                      var objMainBlock = document.getElementById('m-booked-prime-days-99676');
-                      if (objMainBlock !== null) {
-                        var copyBlock = document.getElementById('m-bookew-weather-copy-' + data.results[i].widget_type);
-                        objMainBlock.innerHTML = data.results[i].html_code;
-                        if (copyBlock !== null) objMainBlock.appendChild(copyBlock);
-                      }
-                    }
-                  } else {
-                    alert('data=undefined||data.results is empty');
-                  }
-                }
-                var widgetSrc = "https://widgets.booked.net/weather/info?action=get_weather_info;ver=7;cityID=37228;type=6;scode=124;ltid=3457;domid=582;anc_id=60187;countday=4;cmetric=1;wlangID=4;color=ffffff;wwidth=250;header_color=ffffff;text_color=ffffff;link_color=ffffff;border_form=1;footer_color=ffffff;footer_text_color=ffffff;transparent=1;v=0.0.1";
-                widgetSrc += ';ref=' + widgetUrl;
-                widgetSrc += ';rand_id=99676';
-                var weatherBookedScript = document.createElement("script");
-                weatherBookedScript.setAttribute("type", "text/javascript");
-                weatherBookedScript.src = widgetSrc;
-                document.body.appendChild(weatherBookedScript)
-              </script><!-- weather widget end -->
-
-              <!-- <p class="weather-description"> -->
-
-              </p>
+            <div class="mdl-card__supporting-text mdl-card--expand row" style="align-items: center;">
+              <p id="display_co2" class="col'10">--<sup>&deg;</sup></p>
+              <h1 class="col-2">PPM</h1>              
             </div>
           </div>
         </div>
@@ -133,7 +98,7 @@
               <h2 class="mdl-card__title-text">Conductividad</h2>
               <i class="material-icons">bolt</i>
             </div>
-            <p id="display_cdtv" class="weather-temperature">--</p>
+            <p id="display_cdtv" class="weather-temperature">--</p><h1>PPM</h1>
           </div>
         </div>
       </div>
